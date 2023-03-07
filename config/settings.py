68126c2219fb6da2b9f88d6e1378d835aac1704d
idealprojectuz <idealprojectuz@gmail.com>
2023-03-08 01:11:58 +0500

    python version change

--- config/settings.py
+++ config/settings.py
@@ -17,7 +17,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-MEDIA_ROOT = '/Users/murod/Desktop/documents/django api/todo_project/backend/media'
+MEDIA_ROOT = [BASE_DIR / 'media']
 
 MEDIA_URL = '/media/'
 
@@ -192,7 +192,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
