042bb82de5d2698edea9091c64d4b7303c3e6b4e
idealprojectuz <idealprojectuz@gmail.com>
2023-03-08 12:30:24 +0500

    nmadur

--- videos/urls.py
+++ videos/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 from .views import Videodetail, Videolist, Videoapi, VideoCreate
 urlpatterns = [
     # path('<int:pk>/', Videodetail.as_view()),
-    path('v2', Videolist.as_view()),
+    path('v2/', Videolist.as_view()),
     path('v2/<int:pk>', Videoapi.as_view()),
     path('v2/create', VideoCreate.as_view()),
 ]
