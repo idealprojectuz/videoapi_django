9549bed80bfc40204b9f83942f9354095a349f9c
idealprojectuz <idealprojectuz@gmail.com>
2023-03-08 11:14:06 +0500

    mediaurl

--- config/settings.py
+++ config/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 MEDIA_ROOT = [BASE_DIR / 'media']
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
